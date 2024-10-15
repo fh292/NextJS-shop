1af47b6d1e0e5eb7d556528249c023376b4b4731
f h <falhamad2002@gmail.com>
2024-10-15 14:20:57 +0300

    task 2 is done

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,35 @@
 import Image from "next/image";
 import coffeeShopImg from "./coffeeShopImg.jpg";
+import americano from "./americano.jpg";
+import latte from "./latte.jpg";
+import products from "./products";
+
 export default function Home() {
-  const title = {
-    fontSize: "40px",
-  };
+  const coffeeList = products.map((coffee) => (
+    <div key={coffeeShopImg.id} className="coffee-item">
+      <p>{coffee.name}</p>
+      <Image src={coffee.image} className="w-[200px] h-[250px]" />
+      <p>{coffee.price}</p>
+    </div>
+  ));
   return (
     <>
-      <div className="grid items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
-        <title>app/coffeeShop.js</title>
-        <h1>My Coffee Shop</h1>
-        <p>Where Coffee Meets Comfort</p>
-        <Image src={coffeeShopImg} />
-        <link rel="icon" href="/logo.jpg" sizes="any" />
+      <div className="grid items-center justify-items-center ">
+        <div className="nameDescription">
+          <h1 className="font-bold">My Coffee Shop</h1>
+          <p>Where Coffee Meets Comfort</p>
+        </div>
+        <div
+          style={{
+            width: "400px",
+          }}
+        >
+          <Image src={coffeeShopImg} />
+        </div>
+      </div>
+
+      <div className="coffee-list2 flex justify-center items-center text-center gap-4">
+        {coffeeList}
       </div>
     </>
   );
---
+++ src/app/products.js
@@ -0,0 +1,19 @@
+import Americano from "./americano.jpg";
+import Latte from "./latte.jpg";
+
+const products = [
+  {
+    id: 1,
+    name: "Iced Americano",
+    price: "1 KD",
+    image: Americano,
+  },
+  {
+    id: 1,
+    name: "Iced Latte",
+    price: "1.5 KD",
+    image: Latte,
+  },
+];
+
+export default products;
