8be46a86451bf8286d8c122c86cef329d5b33817
f h <falhamad2002@gmail.com>
2024-10-16 08:52:48 +0300

    design is done

--- src/app/components/ProductItem.js
+++ src/app/components/ProductItem.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function ProductItem({ id, name, image, price }) {
   return (
-    <div>
+    <div className=" transition-transform hover:scale-105">
       <p>{name}</p>
       <Image src={image} className="w-[200px] h-[250px]" alt="coffe_pic" />
       <p>{price}</p>
